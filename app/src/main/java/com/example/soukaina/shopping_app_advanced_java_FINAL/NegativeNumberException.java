470bad82e14bfb132018b15c5aa15e51e3033fab
soukainasalihi <souka-free1@hotmail.fr>
2019-12-26 02:57:44 -0500

    Update NegativeNumberException.java

--- app/src/main/java/com/example/soukaina/shopping_app_advanced_java_FINAL/NegativeNumberException.java
+++ app/src/main/java/com/example/soukaina/shopping_app_advanced_java_FINAL/NegativeNumberException.java
@@ -1,14 +1,10 @@
 package com.example.soukaina.shopping_app_advanced_java_FINAL;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
  * @author Soukaina
  */
+
 // exception class for error handling (negative numbers)
 public class NegativeNumberException extends Exception {
 
