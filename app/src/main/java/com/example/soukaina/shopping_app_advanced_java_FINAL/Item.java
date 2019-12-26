92a488ddfcb9ace5fdd01a9be8f4e4a27b1d7286
soukainasalihi <souka-free1@hotmail.fr>
2019-12-26 02:53:23 -0500

    Update Item.java

--- app/src/main/java/com/example/soukaina/shopping_app_advanced_java_FINAL/Item.java
+++ app/src/main/java/com/example/soukaina/shopping_app_advanced_java_FINAL/Item.java
@@ -7,11 +7,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 /**
  *
  * @author Soukaina
